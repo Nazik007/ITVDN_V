de5e930b6bb25f257be6a87fe95e91068db1a76d
Nazik007 <99899134+Nazik007@users.noreply.github.com>
2022-05-26 21:58:20 +0300

    Update volume.java

--- volume.java
+++ volume.java
@@ -1,11 +1,15 @@
+package homework4;
+
 public class Volume {
     public static void main(String[] args) {
-        int h = 18;
-        int r = 23;
+        double h = 18;
+        double r = 23;
         final double PI = 3.14d;
 
         double v = PI * (r * r) * h;
+        double s = ( 4* PI *r) + 2 * PI * Math.pow(r,2);
 
         System.out.println(v);
+        System.out.println(s);
     }
 }
