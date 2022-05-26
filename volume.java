2aaf203c7cb3ce01c7d4710d830ce8c09e92bd3a
Nazik007 <99899134+Nazik007@users.noreply.github.com>
2022-05-26 15:06:48 +0300

    Create volume.java

---
+++ volume.java
@@ -0,0 +1,11 @@
+public class Volume {
+    public static void main(String[] args) {
+        int h = 18;
+        int r = 23;
+        final double PI = 3.14d;
+
+        double v = PI * (r * r) * h;
+
+        System.out.println(v);
+    }
+}
